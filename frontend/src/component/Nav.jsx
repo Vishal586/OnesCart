b9f7f5f7acd5573b4be1916dae383cec9af937bc
Vishal Kumar <128839643+Vishal586@users.noreply.github.com>
2026-05-18 00:56:01 +0530

    Fix import case for authContext in Nav component

--- frontend/src/component/Nav.jsx
+++ frontend/src/component/Nav.jsx
@@ -12,7 +12,7 @@ import { HiOutlineCollection } from "react-icons/hi";
 import { MdContacts } from "react-icons/md";
 
 import { userDataContext } from "../context/UserContext";
-import { authDataContext } from "../context/authContext";
+import { authDataContext } from "../context/AuthContext";
 import { shopDataContext } from "../context/ShopContext";
 
 import { useNavigate } from "react-router-dom";
