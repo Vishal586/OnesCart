e28973521faa5593570580f655b030989dc766c3
Vishal Kumar <128839643+Vishal586@users.noreply.github.com>
2026-05-18 11:24:16 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,7 @@ let app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
- origin:["http://localhost:5173" , "http://localhost:5174"],
+ origin:["https://onescart.onrender.com" , "http://localhost:5174"],
  credentials:true
 }))
 
